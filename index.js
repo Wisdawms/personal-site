609580ae9d7d27a97c46a852e26598aefdaead49
wisdawms <medianhq@gmail.com>
2024-03-14 07:22:37 +0200

    init

---
+++ index.js
@@ -0,0 +1,77 @@
+const cursor_element = document.querySelectorAll('cursor');
+const about = document.querySelector('#about');
+const aboutContent = document.querySelector('#about-content');
+const contactContent = document.querySelector('#contact-content');
+// const links = document.querySelectorAll('body a');
+
+// links.forEach( (link) => {
+//     link.onclick = () => {
+//         const url = link.getAttribute('link');
+//         console.log(url, link.innerHTML);
+//         const linkBox = new WinBox({
+//             title: link.innerHTML,
+//             background: '#00aa00',
+//             modal: true,
+//             url: url,
+//             onfocus: function () {
+//                 this.setBackground('#00aa00');
+//             },
+//             onblur: function () {
+//                 this.setBackground(idleColor);
+//             }
+//         });
+//         linkBox.maximize();
+//     }
+// });
+
+const idleColor = '777';
+
+about.addEventListener('click', () => {
+    const aboutBox = new WinBox({
+        modal: true,
+        title: 'wisdawms/about',
+        background: '#00aa00',
+        mount: aboutContent,
+        onfocus: function () {
+            this.setBackground('#00aa00');
+        },
+        onblur: function () {
+            this.setBackground(idleColor);
+        }
+    });
+}
+)
+
+contact.addEventListener('click', () => {
+    const contactBox = new WinBox({
+        modal: true,
+        title: 'wisdawms/contact',
+        background: '#00aa00',
+        mount: contactContent,
+        onfocus: function () {
+            this.setBackground('#00aa00');
+        },
+        onblur: function () {
+            this.setBackground(idleColor);
+        }
+    });
+}
+)
+
+cursor_element.forEach((element) => {
+    element.outerHTML = "<span class=\"cursor\">|</span>";
+})
+
+// on window resize, center and resize the open windowBox
+window.addEventListener('resize', () => {
+    const box = document.querySelector('.winbox');
+    if (box) {
+        box.style.maxWidth = '50%';
+        box.style.maxHeight = '50%';
+        box.style.position = "absolute";
+        box.style.top = "50%";
+        box.style.left = "50%";
+        box.style.transform = "translate(-50%, -50%)";
+    }
+
+})
